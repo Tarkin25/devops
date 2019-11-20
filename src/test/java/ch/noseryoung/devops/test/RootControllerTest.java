8a4e85292c4dec1e13f8d5cfe74b4ef2fdc1760c
Severin Weigold <severin.weigold@edu.tbz.ch>
2019-11-20 15:25:38 +0100

    Initial commit

---
+++ src/test/java/ch/noseryoung/devops/test/RootControllerTest.java
@@ -0,0 +1,14 @@
+package ch.noseryoung.devops.test;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+class RootControllerTest {
+
+    @Test
+    void helloWorld() {
+
+        Assertions.assertThat(1).isEqualTo(0);
+
+    }
+}
